9b47dad19643a1cd14551a3b6daa88e6aa510a71
williamdam <59197647+williamdam@users.noreply.github.com>
2020-04-14 15:26:05 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,11 @@ using namespace std;
 int main() {
 	cout << "Hello world!" << endl;
 
+	// Changes by William Dam
+	// Yes, let's print Hello World 25x
+	for (int i = 0; i < 25; i++) {
+		cout << "Hello World!" << endl;
+	}
+
 	return 0;
 }
