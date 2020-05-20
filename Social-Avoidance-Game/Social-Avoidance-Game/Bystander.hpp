9188a102e21fc0b709c8f8bddffcd0ec26e2d8c0
Daniel <50219700+dmesa89@users.noreply.github.com>
2020-05-20 14:40:30 -0700

    changes
    chamnges

---
+++ Social-Avoidance-Game/Social-Avoidance-Game/Bystander.hpp
@@ -0,0 +1,31 @@
+/*********************************************************************
+** Author: William Dam, Daniel Mesa, Tinron Cheung
+** Date: 05-05-2020
+** Description: Bystander.hpp is the class specification file for the
+** Bystander class.  Bystander is a derived class of the Cpu base
+** class.  Member functions defined in separate implementation file.
+*********************************************************************/
+#ifndef Bystander_hpp
+#define Bystander_hpp
+#include <iostream>
+#include <string>
+#include "Die.hpp"
+#include "Entity.hpp"
+
+class Bystander : public Entity {
+
+public:
+	Bystander();				// Default constructor
+	~Bystander();				// Destructor
+	
+	void setInfection(bool n);
+
+private:
+	bool infected;
+	int infectionRadius;		// How close another entity needs to be to get infected
+	bool isInfected();
+	void setInfected(bool);
+
+};
+
+#endif
