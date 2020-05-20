de9fe91fedf0028743c2b366ed184ab5eef5cdad
Daniel <50219700+dmesa89@users.noreply.github.com>
2020-05-20 15:57:54 -0700

    changes
    changes

--- Social-Avoidance-Game/Social-Avoidance-Game/GameBoard.hpp
+++ Social-Avoidance-Game/Social-Avoidance-Game/GameBoard.hpp
@@ -2,6 +2,8 @@
 #define GAMEBOARD_H
 //class imports
 #include <stdio.h>
+#include <cmath>
+#include <vector>
 #include "Player.hpp"
 #include "Bystander.hpp"
 #include "Buildings.hpp"
@@ -9,9 +11,14 @@
 #include "Police.hpp"
 #include "Die.hpp"
 
-#include <cmath>
 
-#include <vector>
+#define C_RED     "\x1b[31m"
+#define C_GREEN   "\x1b[32m"
+#define C_YELLOW  "\x1b[33m"
+#define C_BLUE    "\x1b[34m"
+#define C_MAGENTA "\x1b[35m"
+#define C_CYAN    "\x1b[36m"
+#define C_RESET   "\x1b[0m"
 
 using namespace std;
 
